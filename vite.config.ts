87c7b2d920a13c5adf7014d9f36f499988c351aa
Nickolay Frolenkov <myxomop.k66@gmail.com>
2026-02-09 21:53:47 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/knot-your-plot-workshops/",
   server: {
     host: "::",
     port: 8080,
